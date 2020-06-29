0de782f7733520f0c965b92af905e787fff74f1f
KHAN TANVEER <57847199+khantanveer7@users.noreply.github.com>
2020-06-29 22:49:30 +0530

    initial commit

--- Text_formatter/Text_formatter/asgi.py
+++ Text_formatter/Text_formatter/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for textUtil project.
+ASGI config for Text_formatter project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'textUtil.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Text_formatter.settings')
 
 application = get_asgi_application()
--- Text_formatter/Text_formatter/wsgi.py
+++ Text_formatter/Text_formatter/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for textUtil project.
+WSGI config for Text_formatter project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'textUtil.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Text_formatter.settings')
 
 application = get_wsgi_application()
